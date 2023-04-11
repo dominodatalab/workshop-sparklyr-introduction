f96d0c4eb9ac07bd6964e21ec46fad09210f0db5
Nikolay Manchev <nikolay.manchev@dominodatalab.com>
2023-04-11 03:24:57 -0700

    adding logreg example
    Pushed from Domino: https://prod-field.cs.domino.tech/workspace/nmanchev/workshop-sparklyr?executionId=6426a25c7d6149022bdaa72a

--- ml_example.R
+++ ml_example.R
@@ -10,8 +10,37 @@ sc <- spark_connect(master=url, app_name="HelloWorld")
 # Construct the absolute path to the dataset
 project_name <- Sys.getenv("DOMINO_PROJECT_NAME")
 data_file_location <- paste("file:///mnt/data/", project_name, "/diabetes.csv", sep = "")
-# Read into Spark
-tbl_diabetes <- spark_read_csv(sc, name = "diabetes", path=data_file_location)
+
+# Read into Spark and light FE
+tbl_diabetes <- spark_read_csv(sc, name = "diabetes", path=data_file_location) %>% 
+  ft_one_hot_encoder(input_cols="Pregnancies",
+                     output_cols="Pregnancies_enc") %>%
+  ft_vector_assembler(input_cols = c("Pregnancies", "Glucose", "BMI", "BloodPressure", "SkinThickness", "Insulin", "Age", "DiabetesPedigreeFunction"), 
+                      output_col = "features") %>%
+  ft_standard_scaler(  input_col = "features",
+                       output_col = "features_scaled") %>%
+  select(features, Outcome)
+
+# Preview
+samp <- tbl_diabetes %>% sdf_sample(fraction = .01) %>% collect()
+
+# Partition into training/test
+partitions <- tbl_diabetes %>% 
+              sdf_random_split(training = 0.8, test = 0.2, seed = 1234)
+
+# Fit a logistic regression model
+fit <- partitions$training %>%
+       ml_logistic_regression(features_col = "features", label_col = "Outcome")
+
+# Generate predictions
+pred <- ml_predict(fit, partitions$test) %>% collect()
+
+area_under_ROC <- ml_binary_classification_evaluator(pred, 
+                                                     label_col = "Outcome", 
+                                                     metric_name = "areaUnderROC") 
+
+# Show accuracy
+cat("Are under the ROC curve:", area_under_ROC)
 
 # Disconnect from Spark
 spark_disconnect(sc)
